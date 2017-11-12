42178a7c016b976f996263c23a42c7a734a9851b
Chris Gill <chrisfgill@gmail.com>
2017-11-12 18:58:42 -0500

    pull in conctact from db

--- lambda.js
+++ lambda.js
@@ -3,17 +3,13 @@
 const AWS = require('aws-sdk');
 
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
-const PHONE_NUMBER = '+1...';
-
-const MAX_QUOTES = 20;
 
 const Doc = require('dynamodb-doc');
 const DB = new Doc.DynamoDB();
-const TABLE = "Inspire";
 
 function get_layout (cb) {
     const params = {
-        TableName:TABLE,
+        TableName:"Inspire",
         Key: { 
             "kind":"Layout",
             "id": 0,
@@ -25,40 +21,54 @@ function get_layout (cb) {
     });
 }
 
+function get_contact (id, cb) {
+    const params = {
+        TableName:"InspireSubs",
+        Key: {
+            "id": id,
+        }
+    };
+
+    DB.getItem(params, function (err, data) {
+        if (data) { cb(data.Item); }
+    });
+}
+
 exports.handler = (event, context, callback) => {
     const dblClick = (event["clickType"] == "DOUBLE");
-    
-    get_layout(function (layout) {
-        var pick;
-        var pickn;
-        
-        if (dblClick) { 
-            pick = "Spirit";
-            pickn = layout.kinds["Spirit"]; 
-        }
-        else {
-            var n = Math.floor(Math.random() * Object.keys(layout.kinds).length);
-            pick = Object.keys(layout.kinds)[n];
-            pickn = layout.kinds[pick];
-        }
-        
-        var n = Math.floor(Math.random() * pickn);
-        const paramsDB = {
-            TableName:TABLE, 
-            Key: { 
-                "kind": pick,
-                "id": n
+    get_contact(event.serialNumber, function (contact) {
+        get_layout(function (layout) {
+            var pick;
+            var pickn;
+            
+            if (dblClick) { 
+                pick = "Spirit";
+                pickn = layout.kinds["Spirit"]; 
             }
-        };
-        
-        DB.getItem(paramsDB, function (err,data) {
-            if (data && data.Item && data.Item.quote) {
-                const paramsSNS = {
-                    PhoneNumber: PHONE_NUMBER,
-                    Message: data.Item.quote + " - " + data.Item.person,
-                };
-                SNS.publish(paramsSNS, callback);
+            else {
+                var n = Math.floor(Math.random() * Object.keys(layout.kinds).length);
+                pick = Object.keys(layout.kinds)[n];
+                pickn = layout.kinds[pick];
             }
-        });
+            
+            var n = Math.floor(Math.random() * pickn);
+            const paramsDB = {
+                TableName:"Inspire", 
+                Key: { 
+                    "kind": pick,
+                    "id": n
+                }
+            };
+            
+            DB.getItem(paramsDB, function (err,data) {
+                if (data && data.Item && data.Item.quote) {
+                    const paramsSNS = {
+                        PhoneNumber: contact.phone,
+                        Message: data.Item.quote + " - " + data.Item.person,
+                    };
+                    SNS.publish(paramsSNS, callback);
+                }
+            });
+        }); 
     });
 };
